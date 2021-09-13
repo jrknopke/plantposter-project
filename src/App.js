832f018f121ac8e59c3138fb17f4c399da94929b
Joseph Knopke <josephknopke@gmail.com>
2021-09-12 21:08:57 -0500

    adds some routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route} from "react-router-dom"
+import { Switch } from "react-router"
+import Home from "./components/Home"
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <div>
+          <Switch>
+            <Route path="/home" render = {(routerprops) => <Home/>}/>
+            <Route path="/plants"/>
+            <Route path="plants/new"/>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
