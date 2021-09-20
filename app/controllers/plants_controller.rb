fe5253bab7628d690042e7b009230dcee92e99e0
Joseph Knopke <josephknopke@gmail.com>
2021-09-20 12:59:26 -0500

    updates plant model

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -5,4 +5,26 @@ class PlantsController < ApplicationController
         render json: plants.to_json
     end
 
+    def create
+        plant = Plant.create(plant_params)
+        render json: plant
+    end
+
+    def update
+        plant = Plant.find(params[:id])
+        plant.update_attributes(plant_params)
+        render json: plant
+    end
+
+    def destroy
+        plant = Plant.find(params[:id])
+        plant.destroy
+        # 
+    end
+
+    private
+        def plant_params
+            params.require(:plant).permit(:name, :color)
+        end
+    
 end
--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,3 +1,4 @@
 class Plant < ApplicationRecord
     has_and_belongs_to_many :gardens
+    
 end
