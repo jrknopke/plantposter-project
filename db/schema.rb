ceb20590b79aa4e8242314db55fbbb5083e48d29
Joseph Knopke <josephknopke@gmail.com>
2021-09-20 13:02:57 -0500

    fixes plants table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_09_200049) do
+ActiveRecord::Schema.define(version: 2021_09_20_180138) do
 
   create_table "gardeners", force: :cascade do |t|
     t.string "name"
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(version: 2021_09_09_200049) do
 
   create_table "plants", force: :cascade do |t|
     t.string "name"
-    t.string "type"
     t.string "color"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
